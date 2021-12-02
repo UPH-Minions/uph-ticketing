19a91cb823c03d9f4a89e027907207f95097f88c
RichardAngka <angkawinata25@gmail.com>
2021-12-02 10:04:35 +0700

    Updates

--- src/components/index.jsx
+++
@@ -1,2 +0,0 @@
-export { default as InputCode } from './InputCode';
-export { default as Ticket } from './Ticket';
--- src/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  background-color: #b8cdff;
-  width: 100%;
-  height: 100vh;
-  position: relative;
-`;
